27b3bd823b92d77b8e75489c88e3a9c7ad35eaa8
bwqr <ruzgardeniz.08@hotmail.com>
2022-03-31 14:20:38 +0300

    update configs

--- bin/game-mod.sh
+++ bin/game-mod.sh
@@ -1,14 +1,17 @@
 if [ "$1" == optimal ];
 then
+    echo optimal
     cpupower frequency-set -g performance
     echo auto > /sys/class/drm/card0/device/power_dpm_force_performance_level
-    echo 46500000 > /sys/class/drm/card0/device/hwmon/hwmon2/power1_cap
+    echo 56500000 > /sys/class/drm/card0/device/hwmon/hwmon2/power1_cap
 elif [ "$1" == performance ];
 then
+    echo performance
     cpupower frequency-set -g performance
     echo auto > /sys/class/drm/card0/device/power_dpm_force_performance_level
     echo 96500000 > /sys/class/drm/card0/device/hwmon/hwmon2/power1_cap
 else
+    echo default
     cpupower frequency-set -g schedutil
     echo low > /sys/class/drm/card0/device/power_dpm_force_performance_level
     echo 165000000 > /sys/class/drm/card0/device/hwmon/hwmon2/power1_cap
